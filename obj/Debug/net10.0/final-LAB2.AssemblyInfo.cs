6e2e55ff4cbc2d421723cfdd620df62d6b573e7d
Débora Moyano <57150176+Debora36@users.noreply.github.com>
2026-08-24 12:56:00 -0300

    final

--- obj/Debug/net10.0/final-LAB2.AssemblyInfo.cs
+++ obj/Debug/net10.0/final-LAB2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final-LAB2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2043007a1cb55b75b145d39bc14c73f0f332d9ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("final-LAB2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final-LAB2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
